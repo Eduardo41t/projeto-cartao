8fc5cc6c1f09b15d7defbacf47add7e93c51e8ff
Eduardo41t <142507224+Eduardo41t@users.noreply.github.com>
2024-12-04 13:27:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da inglaterra é Londres'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gostar em Inglês?',
+    'Oi em ingles é like (like)'
+)
